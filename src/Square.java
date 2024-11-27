ddd419cd3ca4162b8522eccef607b60a54d11b37
Mourad0906 <6286227@bdeb.qc.ca>
2024-11-27 15:05:38 -0500

    Initial commit

---
+++ src/Square.java
@@ -0,0 +1,27 @@
+public class Carre extends Rectangle{
+
+    private int side;
+
+    public Carre(int side){
+        this.side = side;
+    }
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWidth(int width) {
+
+    }
+
+    @Override
+    public void setHeight(int height) {
+
+    }
+}
